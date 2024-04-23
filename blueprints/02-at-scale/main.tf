c33f67fbbe847a78c1058a0c5329e080e13fce22
Shaohua Wen <shaohua.wen@gmail.com>
2024-04-23 11:35:03 +0700

    use gp3 as default storage class

--- blueprints/02-at-scale/main.tf
+++ blueprints/02-at-scale/main.tf
@@ -308,6 +308,28 @@ module "eks" {
   tags = local.tags
 }
 
+resource "kubernetes_storage_class_v1" "gp3" {
+  metadata {
+    name = "gp3"
+
+    annotations = {
+      "storageclass.kubernetes.io/is-default-class" = "true"
+    }
+  }
+
+  storage_provisioner    = "ebs.csi.aws.com"
+  allow_volume_expansion = true
+  reclaim_policy         = "Delete"
+  volume_binding_mode    = "WaitForFirstConsumer"
+
+  parameters = {
+    encrypted = "true"
+    fsType    = "ext4"
+    type      = "gp3"
+  }
+
+}
+
 resource "kubernetes_storage_class_v1" "efs" {
 
   metadata {
